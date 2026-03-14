abbda0df40e3d76812ae9791c038dca68295e340
phuccom000 <gdtitans6@gmail.com>
2026-03-14 11:34:59 +0700

    fixed forge version not starting up

--- forge/src/main/java/banduty/kingdomsieges/sounds/ModSounds.java
+++ forge/src/main/java/banduty/kingdomsieges/sounds/ModSounds.java
@@ -1,41 +1,35 @@
 package banduty.kingdomsieges.sounds;
 
 import banduty.kingdomsieges.Kingdomsieges;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-public interface ModSounds {
-    DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(Kingdomsieges.MOD_ID, Registries.SOUND_EVENT);
+public class ModSounds {
 
-    RegistrySupplier<SoundEvent> CANNON_CLOSE = SOUND_EVENTS.register("cannon_close",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "cannon_close")));
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Kingdomsieges.MOD_ID);
 
-    RegistrySupplier<SoundEvent> CANNON_DISTANT = SOUND_EVENTS.register("cannon_distant",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "cannon_distant")));
+    public static final RegistryObject<SoundEvent> CANNON_CLOSE = register("cannon_close");
+    public static final RegistryObject<SoundEvent> CANNON_DISTANT = register("cannon_distant");
+    public static final RegistryObject<SoundEvent> RAM_IMPACT = register("ram_impact");
+    public static final RegistryObject<SoundEvent> ROPE_CHARGE_GN = register("rope_charge_gn");
+    public static final RegistryObject<SoundEvent> ROPE_CHARGE_BR = register("rope_charge_br");
+    public static final RegistryObject<SoundEvent> SIEGE_ENGINE_MOVE = register("siege_engine_move");
+    public static final RegistryObject<SoundEvent> MANGONEL_SHOOT = register("mangonel_shoot");
+    public static final RegistryObject<SoundEvent> TREBUCHET_SHOOT = register("trebuchet_shoot");
 
-    RegistrySupplier<SoundEvent> RAM_IMPACT = SOUND_EVENTS.register("ram_impact",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "ram_impact")));
+    private static RegistryObject<SoundEvent> register(String name) {
+        return SOUND_EVENTS.register(name,
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(Kingdomsieges.MOD_ID, name)));
+    }
 
-    RegistrySupplier<SoundEvent> ROPE_CHARGE_GN = SOUND_EVENTS.register("rope_charge_gn",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "rope_charge_gn")));
-
-    RegistrySupplier<SoundEvent> ROPE_CHARGE_BR = SOUND_EVENTS.register("rope_charge_br",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "rope_charge_br")));
-
-    RegistrySupplier<SoundEvent> SIEGE_ENGINE_MOVE = SOUND_EVENTS.register("siege_engine_move",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "siege_engine_move")));
-
-    RegistrySupplier<SoundEvent> MANGONEL_SHOOT = SOUND_EVENTS.register("mangonel_shoot",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "mangonel_shoot")));
-
-    RegistrySupplier<SoundEvent> TREBUCHET_SHOOT = SOUND_EVENTS.register("trebuchet_shoot",
-            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(Kingdomsieges.MOD_ID, "trebuchet_shoot")));
-
-    static void registerSounds() {
-        SOUND_EVENTS.register();
+    public static void register(IEventBus bus) {
+        SOUND_EVENTS.register(bus);
         Kingdomsieges.LOG.info("Registering Sounds for " + Kingdomsieges.MOD_ID);
     }
 }
