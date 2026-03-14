1a3a1040d071916500aae14e4606f91e24af5280
Banduty <bandutysmods@gmail.com>
2026-03-14 13:39:55 +0100

    Fixed some issues with SiegeSpawnerItem and ModEntities class

--- forge/src/main/java/banduty/kingdomsieges/entity/ModEntities.java
+++ forge/src/main/java/banduty/kingdomsieges/entity/ModEntities.java
@@ -6,10 +6,7 @@ import banduty.kingdomsieges.entity.custom.projectiles.CannonProjectile;
 import banduty.kingdomsieges.entity.custom.projectiles.TrebuchetProjectile;
 import banduty.kingdomsieges.entity.custom.sieges.*;
 
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,74 +19,74 @@ public interface ModEntities {
 
     RegistryObject<EntityType<CannonEntity>> CANNON_ENTITY =
             ENTITY_TYPES.register("cannon_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, CannonEntity::new)
-                            .dimensions(EntityDimensions.fixed(2.0f, 1.5f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.cannonRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(CannonEntity::new, MobCategory.MISC)
+                            .sized(2.0f, 1.5f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.cannonRange)
+                            .updateInterval(1)
+                            .build("cannon_entity")
             );
 
     RegistryObject<EntityType<RibauldequinEntity>> RIBAULDEQUIN_ENTITY =
             ENTITY_TYPES.register("ribauldequin_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, RibauldequinEntity::new)
-                            .dimensions(EntityDimensions.fixed(2.0f, 1.5f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.ribauldequinRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(RibauldequinEntity::new, MobCategory.MISC)
+                            .sized(2.0f, 1.5f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.ribauldequinRange)
+                            .updateInterval(1)
+                            .build("ribauldequin_entity")
             );
 
     RegistryObject<EntityType<BatteringRamEntity>> BATTERING_RAM_ENTITY =
             ENTITY_TYPES.register("battering_ram_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, BatteringRamEntity::new)
-                            .dimensions(EntityDimensions.fixed(2.5f, 2.5f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.batteringRamRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(BatteringRamEntity::new, MobCategory.MISC)
+                            .sized(2.5f, 2.5f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.batteringRamRange)
+                            .updateInterval(1)
+                            .build("battering_ram_entity")
             );
 
     RegistryObject<EntityType<MangonelEntity>> MANGONEL_ENTITY =
             ENTITY_TYPES.register("mangonel_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, MangonelEntity::new)
-                            .dimensions(EntityDimensions.fixed(2.8f, 2.5f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.mangonelRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(MangonelEntity::new, MobCategory.MISC)
+                            .sized(2.8f, 2.5f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.mangonelRange)
+                            .updateInterval(1)
+                            .build("mangonel_entity")
             );
 
     RegistryObject<EntityType<TrebuchetEntity>> TREBUCHET_ENTITY =
             ENTITY_TYPES.register("trebuchet_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, TrebuchetEntity::new)
-                            .dimensions(EntityDimensions.fixed(4.0f, 3.0f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.trebuchetRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(TrebuchetEntity::new, MobCategory.MISC)
+                            .sized(4.0f, 3.0f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.trebuchetRange)
+                            .updateInterval(1)
+                            .build("trebuchet_entity")
             );
 
     RegistryObject<EntityType<MantletEntity>> MANTLET_ENTITY =
             ENTITY_TYPES.register("mantlet_entity", () ->
-                    FabricEntityTypeBuilder.create(MobCategory.MISC, MantletEntity::new)
-                            .dimensions(EntityDimensions.fixed(2.5f, 2f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.mantletRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.of(MantletEntity::new, MobCategory.MISC)
+                            .sized(2.5f, 2f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.mantletRange)
+                            .updateInterval(1)
+                            .build("mantlet_entity")
             );
 
     RegistryObject<EntityType<CannonProjectile>> CANNON_BALL =
             ENTITY_TYPES.register("cannon_ball", () ->
-                    FabricEntityTypeBuilder.<CannonProjectile>create(MobCategory.MISC, CannonProjectile::new)
-                            .dimensions(EntityDimensions.fixed(0.25f, 0.25f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.cannonBallRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.<CannonProjectile>of(CannonProjectile::new, MobCategory.MISC)
+                            .sized(0.25f, 0.25f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.cannonBallRange)
+                            .updateInterval(1)
+                            .build("cannon_ball")
             );
 
     RegistryObject<EntityType<TrebuchetProjectile>> TREBUCHET_PROJECTILE =
             ENTITY_TYPES.register("trebuchet_projectile", () ->
-                    FabricEntityTypeBuilder.<TrebuchetProjectile>create(MobCategory.MISC, TrebuchetProjectile::new)
-                            .dimensions(EntityDimensions.fixed(0.25f, 0.25f))
-                            .trackRangeBlocks(KingdomSiegesForge.CONFIG.common.trebuchetProjectileRange)
-                            .trackedUpdateRate(1)
-                            .build()
+                    EntityType.Builder.<TrebuchetProjectile>of(TrebuchetProjectile::new, MobCategory.MISC)
+                            .sized(0.25f, 0.25f)
+                            .clientTrackingRange(KingdomSiegesForge.CONFIG.common.trebuchetProjectileRange)
+                            .updateInterval(1)
+                            .build("trebuchet_projectile")
             );
 
     static void register(IEventBus bus) {
